83fab22bfc10c0601eee3568018e967361d1c65a
Ingrid_Ramos <ingridrcs@gmail.com>
2022-02-06 23:21:49 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -7,21 +7,53 @@ class Search extends React.Component {
     super();
     this.state = {
       // loading: true,
+      onSearch: true,
+      musicName: '',
     };
+    this.buttonSearch = this.buttonSearch.bind(this);
   }
 
   // componentDidMount() {
   //   const num = 1000;
   //   setTimeout(() => this.setState({ loading: false }), num);
   // }
+  buttonSearch(event) {
+    const { value, name } = event.target;
+    this.setState(() => ({
+      [name]: value }
+    ), () => {
+      const { musicName } = this.state;
+      const numMin = 2;
+      const verifySearch = musicName.length >= numMin;
+      this.setState({ onSearch: !verifySearch });
+    });
+  }
 
   render() {
+    const { onSearch } = this.state;
     // const { loading } = this.state;
     // if (loading) return <Loading />;
     return (
       <>
         <div data-testid="page-search" />
         <Header />
+        <form>
+          <label htmlFor="searchbut">
+            Pesquisar
+            <input
+              data-testid="search-artist-input"
+              name="musicName"
+              onChange={ this.buttonSearch }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ onSearch }
+          >
+            Pesquisar
+          </button>
+        </form>
       </>
     );
   }
